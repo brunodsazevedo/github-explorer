04a2c955b560fbfc18ae06e90d2b11ee919383dc
Bruno Azevedo <bruno.ds.azevedo@gmail.com>
2022-02-17 18:12:41 -0300

    finalizado a transformacao de arquivos js para ts

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,16 +3,14 @@ import RepositoryItem from './RepositoryItem';
 
 import '../styles/repositories.scss';
 
-// https://api.github.com/orgs/rocketseat/repos
-
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform',
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
 }
 
 export default function RepositoryList() {
-  const [ repositories, setRepositories ] = useState([]);
+  const [ repositories, setRepositories ] = useState<Repository[]>([]);
   
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -26,7 +24,7 @@ export default function RepositoryList() {
 
       <ul>
         {repositories.map(repository => (
-          <RepositoryItem key={repository.id} repository={repository} />
+          <RepositoryItem key={repository.name} repository={repository} />
         ))}
       </ul>
     </section>
